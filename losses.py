e1316118c7d5c3c56a63565b524f5108c20f1f75
Vedant <vedantdpthak@iCloud.com>
2017-07-28 20:17:40 -0400

    Implemented crossentropy

--- losses.py
+++ losses.py
@@ -1,4 +1,11 @@
 import numpy as np
+import math
 
-def crossentropy(logits, label_index):
-	
+def crossentropy(logits, labels):
+	sum = 0
+	for i in range(len(logits)):
+		curr = 0
+		curr += labels[i]*math.log(logits[i])
+		curr += (1-labels[i])*math.log(1-logits[i])
+		sum += curr
+	return -sum
